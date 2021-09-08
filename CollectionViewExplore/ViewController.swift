aa90db12e4e9f2661572770f193db665b515eff4
Afir Thes <afirthes@gmail.com>
2021-09-08 08:26:59 +0300

    Insets

--- CollectionViewExplore/ViewController.swift
+++ CollectionViewExplore/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController, UICollectionViewDataSource,
     
     let data: [UIImage] = [#imageLiteral(resourceName: "image2"), #imageLiteral(resourceName: "image4"), #imageLiteral(resourceName: "image1"), #imageLiteral(resourceName: "image3")]
     
+    let edgeInsets: CGFloat = 6
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         imagesCollectionView.dataSource = self
@@ -36,14 +38,22 @@ class ViewController: UIViewController, UICollectionViewDataSource,
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
         let itemsPerRow: CGFloat = 3
-        
-        let collectionViewWidth: CGFloat = collectionView.frame.width
+        let padding: CGFloat = 6
+        let collectionViewWidth: CGFloat = collectionView.frame.width - (padding * (itemsPerRow-1)) - edgeInsets * 2
         let widthPerItem: CGFloat = collectionViewWidth / itemsPerRow
         print(widthPerItem)
         return CGSize(width: widthPerItem, height: widthPerItem)
     }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        
+        return CGFloat(6)
+    }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        
+        return UIEdgeInsets(top: edgeInsets, left: edgeInsets, bottom: edgeInsets, right: edgeInsets)
+    }
 
 }
 
