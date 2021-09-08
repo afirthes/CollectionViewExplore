6ea0255f911a9046b4721a07ad865dd7d15b92fc
Afir Thes <afirthes@gmail.com>
2021-09-08 08:05:46 +0300

    3 in row

---
+++ CollectionViewExplore/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  CollectionViewExplore
+//
+//  Created by Afir Thes on 08.09.2021.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImage: UIImageView!
+    
+}
--- CollectionViewExplore/ViewController.swift
+++ CollectionViewExplore/ViewController.swift
@@ -7,13 +7,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDataSource,
+                      UICollectionViewDelegate,UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet weak var imagesCollectionView: UICollectionView!
+    
+    let data: [UIImage] = [#imageLiteral(resourceName: "image2"), #imageLiteral(resourceName: "image4"), #imageLiteral(resourceName: "image1"), #imageLiteral(resourceName: "image3")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imagesCollectionView.dataSource = self
+        imagesCollectionView.delegate = self
     }
-
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return data.count
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ReusableCell", for: indexPath) as! CollectionViewCell
+        cell.photoImage.image = data[indexPath.row]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let itemsPerRow: CGFloat = 3
+        
+        let collectionViewWidth: CGFloat = collectionView.frame.width
+        let widthPerItem: CGFloat = collectionViewWidth / itemsPerRow
+        print(widthPerItem)
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+    
+    
 
 }
 
